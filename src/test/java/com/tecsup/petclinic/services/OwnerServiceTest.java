bb4f2a82ee4e7af15832a00160e32a4b1cdca8ee
CarlosRoqueM <129903148+CarlosRoqueM@users.noreply.github.com>
2023-06-13 23:22:51 -0500

    commit

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,10 +1,25 @@
 package com.tecsup.petclinic.services;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.h2.server.Service;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootTest
 @Slf4j
 public class OwnerServiceTest {
-	// TO DO
+	
+	@GetMapping("/owners/{id}")
+	ResponseEntity<Owner> findOne(@PathVariable Long id){
+		try {
+			return new ResponseEntity<>(Service.findById(id), HttpStatus.OK)
+		} catch (OwnerNotFoundException e) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND)
+		}
+	}
+	
 }
--- src/test/java/com/tecsup/petclinic/webs/OwnerControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/OwnerControllerTest.java
@@ -1,5 +1,29 @@
 package com.tecsup.petclinic.webs;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
 public class OwnerControllerTest {
 
+	@Test
+	public void testFindOwnerOK() throws Exception {
+		String FIRSTNAME_OWNER = "Leonardo";}
+		String LASTNAME_OWNER = "Ortega";
+		String ADDRESS_OWNER = "Santa anita";
+		String CITY_OWNER = "Lima";
+		String TELEPHONE_OWNER = "98635214";
+		
+		mockMvc.perform(get("owners/1"))
+				.andExpect(status().isOk())
+				.andExpect("$.id", is(1))
+				.andExpect("$.firstname", is(FIRSTNAME_OWNER ))
+				.andExpect("$.lastname", is(LASTNAME_OWNER))
+				.andExpect("$.address", is(ADDRESS_OWNER))
+				.andExpect("$.city", is(CITY_OWNER))
+				.andExpect("$.thelephone", is(TELEPHONE_OWNER));		
+	}
 }
